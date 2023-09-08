79d2b63893fce7d19b768416a4e453ce7224a25f
LamsyA <olamide.adetula.1744@gmail.com>
2023-09-08 14:46:44 +0100

    vercel fix

--- api/app.js
+++ api/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
+
 require('dotenv').config();
 
 const app = express();
 
 
+
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
     const slack_name = req.query.slack_name;
     const track = req.query.track;
