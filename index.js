712db35e68e78c220c041be2bcebd38ec7c223aa
LamsyA <olamide.adetula.1744@gmail.com>
2023-09-08 15:23:35 +0100

    deployment fixed

--- index.js
+++ index.js
@@ -10,7 +10,30 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/home", home);
+app.get('/', (req, res) => {
+    const slack_name = req.query.slack_name;
+    const track = req.query.track;
+
+    if (!slack_name || !track) {
+        return res.status(400).json({ error: "Missing query parameters slack_name and track" });
+    }
+    // Get the current date and time
+    const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
+    const utcTime = new Date().toISOString();
+
+    //  the JSON response
+    const jsonResponse = {
+        slack_name,
+        current_day: currentDay,
+        utc_time: utcTime,
+        track,
+        github_file_url: 'https://github.com/LamsyA/Hngx',
+        github_repo_url: 'https://github.com/lamsya',
+        status_code: 200,
+    };
+
+   return res.json(jsonResponse);
+});;
 
 // Start the Express server
 const port = process.env.PORT || 3004;
